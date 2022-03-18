200c6a406947b880a5a2f3826966489dc23b9b19
Judith <chinonsojudith618@gmail.com>
2022-03-18 16:12:01 +0100

    update

--- contact.js
+++ contact.js
@@ -11,3 +11,13 @@ form.addEventListener('submit',(e)=>{
     sessionStorage.setItem('getData' ,JSON.stringify(details))
     console.log(details)
 })
+
+
+
+// toogle menu display code 
+let toggle = document.querySelector('#toggle');
+let toggleList = document.querySelector('.toggle-list');
+toggle.addEventListener('click', ()=> {
+    toggleList.classList.toggle('toggle-display')
+    
+})
